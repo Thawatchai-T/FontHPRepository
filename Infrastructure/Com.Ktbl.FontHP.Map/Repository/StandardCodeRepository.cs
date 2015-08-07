1700f80856b4135ff1161e13f9d6145439979fb9
nidcha <matao_ome@hotmail.com>
2015-08-07 14:32:15 +0700

    Add init function insert,update,delete,getall,getbyid Edit filter Active = 1
    Signed-off-by: nidcha <matao_ome@hotmail.com>

--- Infrastructure/Com.Ktbl.FontHP.Map/Repository/StandardCodeRepository.cs
+++ Infrastructure/Com.Ktbl.FontHP.Map/Repository/StandardCodeRepository.cs
@@ -14,10 +14,94 @@ namespace Com.Ktbl.FontHP.Map.Repository
             
             using(var session = SessionFactory.OpenStatelessSession())
             using(var tx = session.BeginTransaction()){
-                var result = session.QueryOver<StandardCodes>().Where(Expression.Eq("StandardType", code)).List<StandardCodes>();
+                //var result = session.QueryOver<StandardCodes>().Where(Expression.Eq("StandardType", code)).List<StandardCodes>();
+                var result = session.QueryOver<StandardCodes>().Where(Expression.Eq("StandardType", code)
+                            && Expression.Eq("Active", true)
+                            ).List<StandardCodes>();
                 return result as List<StandardCodes>;
+
             }
 
+        }
+
+        public bool Insert(StandardCodes enitity)
+        {
+            try
+            {
+                var result = this.Insert<StandardCodes>(enitity);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        public void Update(StandardCodes entity)
+        {
+            try
+            {
+                this.Update<StandardCodes>(entity);
+                
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        public void Delete(StandardCodes entity)
+        {
+            try
+            {
+                this.Delete<StandardCodes>(entity);
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        public List<StandardCodes> GetAll()
+        {
+            try
+            {
+                using(var session = SessionFactory.OpenStatelessSession())
+                using (var tx = session.BeginTransaction())
+                {
+                    var result = session.QueryOver<StandardCodes>();
+                    return result as List<StandardCodes>;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+                throw;
+            }
+
+        }
+
+        public List<StandardCodes> GetById(string stdtype, string stdcode)
+        {
+            try
+            {
+                using(var session = SessionFactory.OpenStatelessSession())
+                using(var tx = session.BeginTransaction())
+                {
+                    var result = session.QueryOver<StandardCodes>().Where(Expression.Eq("StandardType", stdtype)
+                            && Expression.Eq("StandardCode", stdcode)
+                        ).List<StandardCodes>();
+                   return result as List<StandardCodes>;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+                throw;
+            }
         }
     }
 }
