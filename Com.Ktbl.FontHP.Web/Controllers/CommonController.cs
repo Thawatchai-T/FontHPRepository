b6cabf6153bdc57f5a8ef9b12e476197b4710fda
Woody <m_oji@live.com>
2015-08-07 11:26:20 +0700

    Initial project web
    Signed-off-by: Woody <m_oji@live.com>

---
+++ Com.Ktbl.FontHP.Web/Controllers/CommonController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Com.Ktbl.FontHP.Domain.Common;
+using Com.Ktbl.FontHP.Map.Repository;
+
+namespace Com.Ktbl.FontHP.Web.Controllers
+{
+    public class CommonController : ApiController
+    {
+        #region properties
+        public StandardCodeRepository StandardCodeRepository { get; set; }
+        public string Title { get; set; }
+
+        #endregion
+
+        // GET api/common
+        public IEnumerable<StandardCodes> Get(string stdcode)
+        {
+            try
+            {
+                var value = Title;
+                var result = StandardCodeRepository.GetStandardCodeWithCode(stdcode);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        
+
+
+        // GET api/common/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/common
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/common/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/common/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Com.Ktbl.FontHP.Web/Utility/NHHelpers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Com.Ktbl.FontHP.Map.Map;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
+namespace Com.Ktbl.FontHP.Web.Utility
+{
+    public class NHHelpers
+    {
+        private string Server { get; set; }
+        private int Port { get; set; }
+        private string Username { get; set; }
+        private string Password { get; set; }
+        private string Database { get; set; }
+
+        public ISessionFactory CreateSessionFactory()
+        {
+            try
+            {
+                var sessionf = Fluently.Configure()
+                    //.ProxyFactoryFactory<ProxyFactoryFactory>()
+                    .Database(MsSqlConfiguration.MsSql2008
+                        .ConnectionString(c => c
+                            .Server(Server)
+                            .Database(Database)
+                            .Username(Username)
+                            .Password(Password)
+                        )
+                    )
+                    .Mappings(m => m.FluentMappings.AddFromAssemblyOf<StandardCodeMap>())
+                    .ExposeConfiguration(c => c.SetProperty("current_session_context_class", "thread_static"))
+                    .BuildSessionFactory();
+                return sessionf;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message); //ex.Message;
+                //  return null;
+            }
+        }
+    }
+}
