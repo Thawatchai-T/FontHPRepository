9bed5f103aa6ea21c5292fac45080049426570c3
Woody <m_oji@live.com>
2015-08-07 11:25:24 +0700

    Initial Project Infrastructure, map, domain
    Signed-off-by: Woody <m_oji@live.com>

---
+++ Infrastructure/Com.Ktbl.FontHP.Map/Repository/StandardCodeRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Com.Ktbl.FontHP.Domain.Common;
+using NHibernate.Criterion;
+
+namespace Com.Ktbl.FontHP.Map.Repository
+{
+    public class StandardCodeRepository: NhRepository
+    {
+        public List<StandardCodes> GetStandardCodeWithCode(string code){
+            
+            using(var session = SessionFactory.OpenStatelessSession())
+            using(var tx = session.BeginTransaction()){
+                var result = session.QueryOver<StandardCodes>().Where(Expression.Eq("StandardType", code)).List<StandardCodes>();
+                return result as List<StandardCodes>;
+            }
+
+        }
+    }
+}
