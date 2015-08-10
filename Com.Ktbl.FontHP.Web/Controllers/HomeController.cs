c90aede14347aa33d91bbc31f741b716080a855b
nidcha <matao_ome@hotmail.com>
2015-08-10 10:29:11 +0700

    Integrate Design
    Signed-off-by: nidcha <matao_ome@hotmail.com>

---
+++ Com.Ktbl.FontHP.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Com.Ktbl.FontHP.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ Com.Ktbl.FontHP.Web/resources/Readme.md
@@ -0,0 +1,3 @@
+# ext-theme-neptune-eced4e28-8031-4275-b7e5-57e6566dfcca/resources
+
+This folder contains static resources (typically an `"images"` folder as well).
