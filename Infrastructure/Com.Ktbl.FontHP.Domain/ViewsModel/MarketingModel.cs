a14cbb8bd99b66f408cd656c3f347c710464a2e5
Woody <m_oji@live.com>
2015-08-28 14:04:09 +0700

    add viewmode MarketingModel
    Signed-off-by: Woody <m_oji@live.com>

---
+++ Infrastructure/Com.Ktbl.FontHP.Domain/ViewsModel/MarketingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Ktbl.FontHP.Domain.ViewsModel
+{
+    public class MarketingModel
+    {
+        public int id { get; set; }
+        public string MarketingCode { get; set; }
+        public string MarketingName { get; set; }
+        public string PhoneNo { get; set; }
+
+    }
+}
